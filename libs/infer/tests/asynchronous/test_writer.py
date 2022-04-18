2f981a6d85f0028b3a53dc9c1f06c885f21f7724
Alec Gunny <alec.gunny@gmail.com>
2022-04-18 10:19:57 -0700

    adding async writer tests

--- libs/infer/tests/asynchronous/test_writer.py
+++ libs/infer/tests/asynchronous/test_writer.py
@@ -32,46 +32,13 @@ def aggregation_steps(request):
 
 
 @pytest.fixture
-def writer(
+def dataset(
     write_dir,
-    inference_sampling_rate,
-    sample_rate,
-    memory,
-    look_ahead,
     aggregation_steps,
-    postprocessor,
-):
-    writer = FrameWriter(
-        write_dir,
-        channel_name="test_channel",
-        inference_sampling_rate=inference_sampling_rate,
-        sample_rate=sample_rate,
-        strain_q=Queue(),
-        postprocessor=postprocessor,
-        memory=memory,
-        look_ahead=look_ahead,
-        aggregation_steps=aggregation_steps,
-        output_name="aggregator",
-        name="writer",
-    )
-    writer.logger = logging.getLogger()
-
-    yield writer
-    writer.in_q.close()
-    writer.out_q.close()
-
-
-def test_writer(
+    sample_rate,
+    inference_sampling_rate,
     num_frames,
     frame_length,
-    sample_rate,
-    look_ahead,
-    validate_frame,
-    start_timestamp,
-    write_dir,
-    inference_sampling_rate,
-    aggregation_steps,
-    writer,
 ):
     # build a dummy arange array for testing, but
     # add negative elements to the start that we
@@ -87,8 +54,7 @@ def test_writer(
     # that will be our dummy "witnesses"
     updates = np.split(x, (len(x) / sample_rate) * inference_sampling_rate)
 
-    # pass the strains and dummy filenames
-    # into the writer's strain_q
+    strain_packages = update_packages = []
     for i, strain in enumerate(strains):
         fname = f"{start_timestamp + i}-{frame_length}.gwf"
         witness_fname = write_dir / ("witness-" + fname)
@@ -99,7 +65,7 @@ def test_writer(
         # and strain filenames) and the strain frame array
         fnames = (str(witness_fname), str(strain_fname))
         package = (fnames, strain)
-        writer.strain_q.put(package)
+        strain_packages.append(package)
 
         # need to be able to os.stat witness fnames for latency
         with open(witness_fname, "w"):
@@ -109,39 +75,63 @@ def test_writer(
     # into the writer's main in_q for processing
     for i, update in enumerate(updates):
         package = Package(update, i)
-        writer.in_q.put({writer.output_name: package})
+        update_packages.append(package)
+    return strain_packages, update_packages
 
-    # add in a `None` for our purposes here
-    # so that we know when we're done
-    writer.in_q.put(None)
 
-    # now simulate the main `run` loop of
-    # a stillwater process
-    while True:
-        package = writer.get_package()
-        if package is None:
-            break
-        writer.process(package)
+def pass_dataset(writer, dataset):
+    strains, updates = dataset
+    for strain in strains:
+        writer.strain_q.put(strain)
+    for update in updates:
+        writer.in_q.put({writer.output_name: update})
 
-    # now ensure that the writer wrote each one of
-    # the corresponding frames in order and run
-    # our standard frame validater to ensure that
-    # the frames have the right shape and content
-    for i in range(num_frames):
-        try:
-            fname, latency = writer.out_q.get_nowait()
-        except Empty:
-            # if the filter lead time is >= the
-            # frame length, the (num_frames - 1)th frame
-            # won't have gotten processed because
-            # there wasn't sufficient data
-            if look_ahead >= frame_length:
-                break
 
-            raise ValueError(
-                "Expected {} frames but only found {}".format(num_frames, i)
-            )
+@pytest.fixture(scope="function")
+def writer(
+    write_dir,
+    inference_sampling_rate,
+    sample_rate,
+    memory,
+    look_ahead,
+    aggregation_steps,
+    postprocessor,
+):
+    return FrameWriter(
+        write_dir,
+        channel_name="test_channel",
+        inference_sampling_rate=inference_sampling_rate,
+        sample_rate=sample_rate,
+        strain_q=Queue(),
+        postprocessor=postprocessor,
+        memory=memory,
+        look_ahead=look_ahead,
+        aggregation_steps=aggregation_steps,
+        output_name="aggregator",
+        name="writer",
+    )
 
+
+@pytest.fixture
+def sync_writer(writer, dataset):
+    writer.logger = logging.getLogger()
+    try:
+        pass_dataset(writer, dataset)
+        yield writer
+    finally:
+        writer.in_q.close()
+        writer.out_q.close()
+
+
+@pytest.fixture
+def async_writer(writer, dataset):
+    with writer:
+        pass_dataset(writer, dataset)
+        yield writer
+
+
+def validate_fname(validate_frame, start_timestamp, frame_length, num_frames):
+    def _validate_fname(fname, i):
         # make sure the frame name is correct
         _, t0, length = parse_frame_name(fname)
         assert t0 == (start_timestamp + i)
@@ -156,7 +146,7 @@ def test_writer(
         # production, so just exit the loop because
         # the validation will fail
         if (i + 1) == num_frames:
-            break
+            return True
 
         # now read in the written frame and validate
         # that it matches our expectations
@@ -165,7 +155,68 @@ def test_writer(
         assert (ts.dt.value * len(ts)) == length
 
         validate_frame(ts.value, i)
+        return False
+
+    return _validate_fname
+
+
+def test_writer(
+    num_frames,
+    frame_length,
+    look_ahead,
+    validate_fname,
+    start_timestamp,
+    sync_writer,
+    dataset,
+):
+    writer = sync_writer
+
+    # add in a `None` for our purposes here
+    # so that we know when we're done
+    writer.in_q.put(None)
+
+    # now simulate the main `run` loop of
+    # a stillwater process
+    for package in iter(writer.get_package, None):
+        writer.process(package)
+
+    # now ensure that the writer wrote each one of
+    # the corresponding frames in order and run
+    # our standard frame validater to ensure that
+    # the frames have the right shape and content
+    for i in range(num_frames):
+        try:
+            fname, _ = writer.out_q.get_nowait()
+        except Empty:
+            # if the filter lead time is >= the
+            # frame length, the (num_frames - 1)th frame
+            # won't have gotten processed because
+            # there wasn't sufficient data
+            if look_ahead >= frame_length:
+                break
+
+            raise ValueError(
+                "Expected {} frames but only found {}".format(num_frames, i)
+            )
+        validate_fname(fname, i)
 
     # ensure that the queue is empty
     with pytest.raises(Empty):
         writer.out_q.get_nowait()
+
+
+def test_writer_async(
+    num_frames,
+    frame_length,
+    look_ahead,
+    validate_fname,
+    start_timestamp,
+    async_writer,
+    dataset,
+):
+    writer = async_writer
+    for i, (fname, _) in enumerate(writer):
+        if validate_fname(fname, i):
+            break
+        elif (i + 2) == num_frames and look_ahead >= frame_length:
+            break

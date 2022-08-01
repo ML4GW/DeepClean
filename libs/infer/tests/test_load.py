199751e0032a41af1fda94de6e44b1afbcb4628e
Alec Gunny <alec.gunny@gmail.com>
2022-08-01 12:56:30 -0700

    adding iterator tests

--- libs/infer/tests/test_load.py
+++ libs/infer/tests/test_load.py
@@ -50,3 +50,62 @@ def test_load_frame(write_dir, channels, ts, sample_rate):
         assert result.shape == (len(channels), len(x) / 2)
         for i, row in enumerate(result):
             assert (row[5:-5] == (x[::2][5:-5] + i)).all()
+
+
+def test_frame_iterator(
+    write_dir, channels, ts, sample_rate, inference_sampling_rate
+):
+    # TODO: turn this into a check in loader __init__
+    if sample_rate < inference_sampling_rate:
+        return
+
+    if isinstance(channels, str):
+        num_channels = 1
+    else:
+        num_channels = len(channels)
+
+    # create a neat organized dummy array to check against
+    num_frames = 10
+    data = (
+        np.arange(num_frames * sample_rate * num_channels)
+        .reshape(num_channels, num_frames, -1)
+        .transpose(1, 0, 2)
+    )
+    if isinstance(channels, str):
+        data = data[:, 0]
+
+    # write frames using the dummy data
+    fnames = []
+    for i, frame in enumerate(data):
+        if isinstance(channels, str):
+            timeseries = ts(frame, channels)
+        else:
+            assert len(frame.shape) == 2
+            timeseries = TimeSeriesDict(
+                {i: ts(j, i) for i, j in zip(channels, frame)}
+            )
+
+        fname = write_dir / f"{i}.gwf"
+        timeseries.write(fname)
+        fnames.append(fname)
+
+    crawler = fnames
+    it = load.frame_iterator(
+        crawler, channels, sample_rate, inference_sampling_rate
+    )
+    stride = int(sample_rate // inference_sampling_rate)
+    num_updates = int(num_frames * sample_rate // stride)
+
+    for i, (x, sequence_end) in enumerate(it):
+        if isinstance(channels, str):
+            assert x.shape == (stride,)
+            assert (x == np.arange(stride) * (i + 1)).all()
+        else:
+            assert x.shape == (num_channels, stride)
+            for j, row in enumerate(x):
+                start = i * num_channels + j
+                stop = start + stride
+                assert (x == np.arange(start, stop)).all()
+
+        assert sequence_end == ((i + 1) == num_updates)
+    assert sequence_end

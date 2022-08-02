955eea6f9637c5a87dcb24720a39d8fcdf818653
Alec Gunny <alec.gunny@gmail.com>
2022-08-02 08:56:36 -0700

    adding blank test so workflow will work

---
+++ projects/sandbox/train/tests/test_train.py
@@ -0,0 +1,7 @@
+def test_train():
+    # TODO: how to make a test that doesn't
+    # just replicate the functionality of the
+    # gwftools io tests? I think all you really
+    # need is to test that the valid fracs come
+    # out properly
+    pass

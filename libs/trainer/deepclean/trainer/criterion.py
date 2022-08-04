c76a14afc14bf4202e5379af231f246f6abc797c
Alec Gunny <alec.gunny@gmail.com>
2022-08-04 09:32:25 -0700

    adding cross power spectral functionality

--- libs/trainer/deepclean/trainer/criterion.py
+++ libs/trainer/deepclean/trainer/criterion.py
@@ -9,6 +9,124 @@ from scipy.signal.spectral import _median_bias
 from deepclean.signal.filter import normalize_frequencies
 
 
+def _validate_shapes(x: torch.Tensor, y: Optional[torch.Tensor]):
+    if y is not None:
+        # acceptable combinations of shapes:
+        # x: time, y: time
+        # x: channel x time, y: time OR channel x time
+        # x: batch x channel x time,
+        # y: batch x channel x time OR batch x time
+        if x.shape[-1] != y.shape[-1]:
+            raise ValueError(
+                "Time dimensions of x and y tensors must "
+                "be the same, found {} and {}".format(x.shape[-1], y.shape[-1])
+            )
+        elif x.ndim == 1 and not y.ndim == 1:
+            raise ValueError(
+                "Can't compute cross spectral density of "
+                "1D tensor x with {}D tensor y".format(y.ndim)
+            )
+        elif x.ndim > 1 and y.ndim == x.ndim:
+            if not (y.shape == x.shape).all():
+                raise ValueError(
+                    "If x and y tensors have the same number "
+                    "of dimensions, shapes must fully match. "
+                    "Found shapes {} and {}".format(x.shape, y.shape)
+                )
+        elif x.ndim > 1 and y.ndim != (x.ndim - 1):
+            raise ValueError(
+                "Can't compute cross spectral density of "
+                "tensors with shapes {} and {}".format(x.shape, y.shape)
+            )
+        elif x.ndim > 2 and y.shape[0] != x.shape[0]:
+            raise ValueError(
+                "If x is a 3D tensor and y is a 2D tensor, "
+                "0th batch dimensions must match, but found "
+                "values {} and {}".format(x.shape[0], y.shape[0])
+            )
+
+
+def _median(x, axis):
+    return torch.quantile(x, q=0.5, axis=axis)
+
+
+def _fast_csd(
+    x: torch.Tensor,
+    nperseg: int,
+    nstride: int,
+    window: torch.Tensor,
+    scale: torch.Tensor,
+    average: str,
+    y: Optional[torch.Tensor] = None,
+):
+    # for fast implementation, use the built-in stft
+    # module along the last dimension and let torch
+    # handle the heavy lifting under the hood
+    if x.ndim > 2:
+        # stft only works on 2D input, so roll the
+        # channel dimension out along the batch
+        batch_size = x.shape[0]
+        num_channels = x.shape[1]
+        x = x.reshape(-1, x.shape[-1])
+
+        if y is not None and y.ndim == 3:
+            y = y.reshape(-1, x.shape[-1])
+    else:
+        batch_size = None
+
+    x = x - x.mean(axis=-1, keepdims=True)
+    fft = torch.stft(
+        x,
+        n_fft=nperseg,
+        hop_length=nstride,
+        window=window,
+        normalized=False,
+        center=False,
+        return_complex=True,
+    )
+    if y is not None:
+        y = y - y.mean(axis=-1, keepdims=True)
+        y_fft = torch.stft(
+            y,
+            n_fft=nperseg,
+            hop_length=nstride,
+            window=window,
+            normalized=False,
+            center=False,
+            return_complex=True,
+        )
+        if batch_size is not None and fft.shape[0] > y_fft.shape[0]:
+            fft = fft.reshape(batch_size, num_channels, -1)
+            y_fft = y_fft.view(batch_size, 1, fft.shape[-1])
+
+        fft = torch.conj(fft) * y_fft
+    else:
+        fft = fft.abs() ** 2
+
+    fft *= scale
+    if nperseg % 2:
+        fft[..., 1:] *= 2
+    else:
+        fft[..., 1:-1] *= 2
+
+    if average == "mean":
+        fft = fft.mean(axis=-1)
+    else:
+        bias = _median_bias(fft.shape[-1])
+        if y is not None:
+            real_median = _median(fft.real, -1)
+            imag_median = _median(fft.imag, -1)
+            fft = real_median + 1j * imag_median
+        else:
+            fft = _median(fft, -1)
+
+        fft /= bias
+
+    if fft.ndim == 2 and batch_size is not None:
+        fft = fft.reshape(batch_size, num_channels, -1)
+    return fft
+
+
 class TorchWelch(nn.Module):
     def __init__(
         self,
@@ -48,63 +166,39 @@ class TorchWelch(nn.Module):
         self.device = device
         self.fast = fast
 
-    def forward(self, x):
-        if x.shape[-1] < self.nperseg:
-            raise ValueError(
-                "Not enough samples {} in input for number "
-                "of fft samples {}".format(x.shape[-1], self.nperseg)
-            )
-        elif x.ndim > 3:
-            raise ValueError(
-                f"Can't perform welch transform on tensor with shape {x.shape}"
-            )
+    def forward(self, x: torch.Tensor, y: Optional[torch.Tensor] = None):
+        tensors = [x]
+        if y is not None:
+            tensors.append(y)
+
+        for tensor, name in zip(tensors, ["x", "y"]):
+            if tensor.shape[-1] < self.nperseg:
+                raise ValueError(
+                    "Number of samples {} in input {} is insufficient "
+                    "for number of fft samples {}".format(
+                        tensor.shape[-1], name, self.nperseg
+                    )
+                )
+            elif tensor.ndim > 3:
+                raise ValueError(
+                    "Can't perform welch transform on tensor {} "
+                    "with shape {}".format(name, tensor.shape)
+                )
+        _validate_shapes(x, y)
 
         if self.fast:
-            # for fast implementation, use the built-in stft
-            # module along the last dimension and let torch
-            # handle the heavy lifting under the hood
-            if x.ndim > 2:
-                batch_size = x.shape[0]
-                num_channels = x.shape[1]
-                x = x.reshape(-1, x.shape[-1])
-            else:
-                batch_size = None
-
-            x = x - x.mean(axis=-1, keepdims=True)
-            fft = torch.stft(
+            return _fast_csd(
                 x,
-                n_fft=self.nperseg,
-                hop_length=self.nstride,
+                y=y,
+                nperseg=self.nperseg,
+                nstride=self.nstride,
                 window=self.window,
-                normalized=False,
-                center=False,
-                return_complex=True,
+                scale=self.scale,
+                average=self.average,
             )
-            fft = fft.abs() ** 2
 
-            if self.nperseg % 2:
-                slc = slice(1, None)
-                fft[:, 1:] *= 2
-            else:
-                slc = slice(1, -1)
-
-            # need to check if x was 1D since that won't have
-            # a 0th dimension corresponding to batch or channel
-            if x.ndim == 1:
-                fft[slc] *= 2
-            else:
-                fft[:, slc] *= 2
-            fft *= self.scale
-
-            if self.average == "mean":
-                fft = fft.mean(axis=-1)
-            else:
-                bias = _median_bias(fft.shape[-1])
-                fft = torch.quantile(fft, q=0.5, axis=-1) / bias
-
-            if batch_size is not None:
-                fft = fft.reshape(batch_size, num_channels, -1)
-            return fft
+        if y is not None:
+            raise NotImplementedError
 
         # for non-fast implementation, we need to unfold
         # the tensor along the time dimension ourselves
@@ -125,7 +219,7 @@ class TorchWelch(nn.Module):
         # the time dimensions so that we can unfold it exactly
         num_segments = (x.shape[-1] - self.nperseg) // self.nstride + 1
         stop = (num_segments - 1) * self.nstride + self.nperseg
-        x = x[:, :, :, :stop]
+        x = x[..., :stop]
 
         # unfold x into overlapping segments and detrend and window
         # each one individually before computing the rfft. Unfold
@@ -159,7 +253,7 @@ class TorchWelch(nn.Module):
             return fft.mean(axis=-2)
         else:
             bias = _median_bias(num_segments)
-            return torch.quantile(fft, q=0.5, axis=-2) / bias
+            return _median(fft, -2) / bias
 
 
 class PSDLoss(nn.Module):

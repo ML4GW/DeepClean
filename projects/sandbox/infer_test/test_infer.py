c8b137539574e87b53e480f61ed9f38fca68520d
Alec Gunny <alec.gunny@gmail.com>
2022-10-21 11:22:56 -0700

    adding hermes inference

--- projects/sandbox/infer_test/test_infer.py
+++ projects/sandbox/infer_test/test_infer.py
@@ -9,13 +9,13 @@ from gwpy.timeseries import TimeSeries
 from deepclean.architectures import DeepCleanAE
 from deepclean.export import PrePostDeepClean
 from deepclean.signal.filter import BandpassFilter
-
-# from hermes.quiver.streaming_input import Snapshotter
-# from hermes.quiver.streaming_output import OnlineAverager
+from hermes.quiver.streaming_input import Snapshotter
+from hermes.quiver.streaming_output import OnlineAverager
 
 NUM_SECONDS = 11
 STRIDE = 32
 SAMPLE_RATE = 4096
+BATCH_SIZE = 1
 
 with open("../pyproject.toml", "r") as f:
     config = toml.load(f)
@@ -72,3 +72,48 @@ local_cleaned = strain - local_prediction
 local_asd = local_cleaned.asd(fftlength=2, window="hann", method="median")
 local_asdr = local_asd / strain_asd
 print(local_asdr.crop(55, 65))
+
+snapshotter = Snapshotter(
+    SAMPLE_RATE,
+    stride_size=STRIDE,
+    batch_size=BATCH_SIZE,
+    channels_per_snapshot=21,
+).to("cuda")
+snapshot = torch.zeros(1, 21, SAMPLE_RATE).to("cuda")
+
+averager = OnlineAverager(
+    update_size=STRIDE,
+    batch_size=BATCH_SIZE,
+    num_updates=num_average,
+    num_channels=None,
+).to("cuda")
+average_shape = STRIDE * (num_average + BATCH_SIZE - 1)
+online_average = torch.zeros((average_shape,)).to("cuda")
+update_idx = torch.zeros((1,)).to("cuda")
+
+num_kernels = x.shape[-1] // STRIDE
+num_batches = num_kernels // BATCH_SIZE
+
+hermes_results = np.array([])
+for i in range(num_batches):
+    slc = slice(i * STRIDE * BATCH_SIZE, (i + 1) * STRIDE * BATCH_SIZE)
+    update = torch.Tensor(x[None, :, slc]).to("cuda")
+
+    with torch.no_grad():
+        kernel, snapshot = snapshotter(update, snapshot)
+        prediction = nn(kernel)
+        averaged, online_average, update_idx = averager(
+            prediction, online_average, update_idx
+        )
+    averaged = averaged.cpu().numpy()[0]
+    hermes_results = np.append(hermes_results, averaged)
+
+hermes_results = hermes_results[num_average * STRIDE :]
+hermes_prediction = bpf(hermes_results)
+hermes_prediction = hermes_prediction[
+    SAMPLE_RATE : (NUM_SECONDS - 2) * SAMPLE_RATE
+]
+hermes_clean = strain - hermes_prediction
+hermes_asd = hermes_clean.asd(fftlength=2, window="hann", method="median")
+hermes_asdr = hermes_asd / strain_asd
+print(hermes_asdr.crop(55, 65))
